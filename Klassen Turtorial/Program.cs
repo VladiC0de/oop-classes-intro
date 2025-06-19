bf278403d1f9d4be6bef28ece3339ac848d7a683
Vladyslav Astashyn <vladyk@bluewin.ch>
2025-06-19 22:40:10 +0200

    Initial commit: Einführung in Klassen (OOP C#)

---
+++ Klassen Turtorial/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+
+using System;
+
+class Person
+{
+    // Attribute(Variablen)+ Eigenschaften
+    public string Name { get; set; }
+    public int Alter { get; set; }
+    public string Vorname { get; set; }
+    public string Adresse { get; set; }
+
+    // Methode
+    public void SagHallo()
+    {
+        Console.WriteLine($"Hallo, ich heiße {Vorname} {Name} und bin {Alter} Jahre alt.");
+    }
+
+    // Methode
+    public string GetAdresse()
+    {
+        return Adresse;
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Einführung in Klassen (C#)
+
+Ein einfaches Tutorial-Projekt zur Einführung in die objektorientierte Programmierung mit Klassen in C#.
+
+## Inhalte
+
+- Definition und Verwendung eigener Klassen
+- Felder, Eigenschaften, Methoden
+- Objektinstanziierung und Zugriff
+
+## Technologien
+
+- C#
+- .NET Framework / .NET Core (je nach Setup)
+- Windows Forms (falls GUI enthalten)
+
+## Autor
+
+[VladiC0de](https://github.com/VladiC0de)
